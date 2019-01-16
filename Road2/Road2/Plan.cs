9f33508fee24b88b2f568d199b6e90b3775bd7ed
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-16 23:39:15 +0100

    ajout de Plan

--- Road2/Road2/Plan.cs
+++ Road2/Road2/Plan.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Road2
 {
     public class Plan
     {
+        // grille contenant la liste des cases
+        List<ICase> grille;
         public Plan()
         {
         }
