4430b05aac67754ba466046ffadf69fe89b0d967
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-16 23:39:54 +0100

    modif de programm

--- Road2/Road2/Program.cs
+++ Road2/Road2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Road2
 {
@@ -7,6 +11,31 @@ namespace Road2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public List<ICase> CreationGrille(List<(int lig, int col, string type)> list)
+        {
+            List<ICase> grille = new List<ICase>();
+
+            foreach ((int lig, int col, string type) caseGrille in list)
+            {
+                switch (caseGrille.type)
+                {
+                    case "couloir":
+                        grille.Add(new CaseCouloir(caseGrille.lig, caseGrille.col));
+                        break;
+                    case "escalier":
+                        grille.Add(new CaseEscalier(caseGrille.lig, caseGrille.col));
+                        break;
+                    case "cle":
+                        grille.Add(new PtCle(caseGrille.lig, caseGrille.col));
+                        break;
+                    default:
+                        grille.Add(new CaseNoeud(caseGrille.lig, caseGrille.col));
+                        break;
+                }
+            }
+            return grille;
         }
     }
 }
