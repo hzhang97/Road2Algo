a23b1ae17c984f69f2c48a098d4b1919474b7c30
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-16 23:38:43 +0100

    ajout ICase

--- Road2/Road2/ICase.cs
+++ Road2/Road2/ICase.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+
 namespace Road2
 {
     public interface ICase
     {
+        // coordonnées du point
+        int Lig { get; set; }
+        int Col { get; set; }
+        // nature de la case
+        Boolean Couloir { get; set; }
+        Boolean Noeud { get; set; }
+        Boolean Escalier { get; set; }
+        Boolean PtCle { get; set; }
     }
 }
