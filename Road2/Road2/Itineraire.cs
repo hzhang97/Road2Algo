ec756e64cf9180c9a9619b1ab6fe69ff38b9f725
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-19 13:38:46 +0100

    ajout de la classe itinéraire pour contenir un itinéraire possible

---
+++ Road2/Road2/Itineraire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Road2
+{
+    public class Itineraire
+    {
+        List<CaseCouloir> iti;
+        int distance;
+
+        public Itineraire()
+        {
+            iti = new List<CaseCouloir>();
+            distance = 0;
+        }
+
+        public int Distance
+        {
+            get { return this.distance; }
+        }
+
+        public void AddCase(CaseCouloir caseC)
+        {
+            iti.Add(caseC);
+            distance++;
+        }
+    }
+}
---
+++ Road2/Road2/ListNoeud.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Road2
+{
+    public class ListNoeud
+    {
+        public ListNoeud()
+        {
+        }
+    }
+}
