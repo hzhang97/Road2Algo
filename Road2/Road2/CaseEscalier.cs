dfa0ece9c3c704cadd8b2c6a2f17b9e362db88af
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-19 13:40:19 +0100

    ajout d'attribut

--- Road2/Road2/CaseEscalier.cs
+++ Road2/Road2/CaseEscalier.cs
@@ -3,9 +3,17 @@ namespace Road2
 {
     public class CaseEscalier : Case
     {
+        CaseCouloir ptCouloir;
+
         public CaseEscalier(int lig, int col) : base(lig, col)
         {
             this.Escalier = true;
+        }
+
+        public CaseCouloir PtCouloir
+        {
+            get { return this.ptCouloir; }
+            set { this.ptCouloir = value; }
         }
     }
 }
