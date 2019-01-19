64176af908fc4a3a404810140a206a893afe0382
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-19 13:40:07 +0100

    ajout d'attribut

--- Road2/Road2/CaseCouloir.cs
+++ Road2/Road2/CaseCouloir.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Road2
 {
     public class CaseCouloir : Case
     {
+        List<CaseNoeud> noeudVoisin;
+
         public CaseCouloir(int lig, int col) : base (lig, col)
         {
             this.Couloir = true;
+        }
+
+        public List<CaseNoeud> NoeudVoisin
+        {
+            get { return this.noeudVoisin; }
+            set { this.noeudVoisin = value; }
         }
     }
 }
