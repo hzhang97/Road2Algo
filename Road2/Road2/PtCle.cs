b5bc63426af1099fda56e860964498d79f451d8b
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-19 13:40:42 +0100

    ajout d'attribut

--- Road2/Road2/PtCle.cs
+++ Road2/Road2/PtCle.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Road2
 {
     public class PtCle : Case
     {
+        CaseCouloir ptCouloir;//= new CaseCouloir()
+
         public PtCle(int lig, int col) : base(lig, col)
         {
             this.PtCle = true;
+        }
+
+        public CaseCouloir PtCouloir
+        {
+            get { return this.ptCouloir; }
+            set { this.ptCouloir = value; }
         }
     }
 }
