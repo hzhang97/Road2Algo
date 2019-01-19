a2ce1b3428bf0097817e646836057d2ad7b93860
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2019-01-19 13:39:35 +0100

    ajout de la classe ListNoeud pour pouvoir générer une liste de liste

--- Road2/Road2/ListNoeud.cs
+++ Road2/Road2/ListNoeud.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Road2
 {
     public class ListNoeud
     {
+        List<CaseNoeud> liste;
+        Boolean statusFinis;
+
         public ListNoeud()
         {
+            liste = new List<CaseNoeud>();
+            statusFinis = false;
         }
+
+        public List<CaseNoeud> Liste
+        {
+            get { return this.liste; }
+        }
+        public Boolean StatutFinis
+        {
+            get { return this.statusFinis; }
+            set { this.statusFinis = value; }
+        }
+
+        public void AddNoeud(CaseNoeud noeud)
+        {
+            liste.Add(noeud);
+        }
+
+        //public static explicit operator ListNoeud(void v)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
